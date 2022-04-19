1ac7efdfd047b95d08bc598286adaa8b6873ed4e
jrbergen <6237646+jrbergen@users.noreply.github.com>
2022-04-19 23:42:24 +0200

    Dataset loading for EPLV dataset (solar panels) (#3)
    * Added `mypy` dev dependency for static analysis.
    
    * Moved `version_check` to separate `compatibility_check.py` module as to not clutter `main.py` with arbitrary stuff.
    
    * removed `path_helpers.add_default_dataset_urls_yaml_file` which used to download datasets defined by a URL and other required data to pre-process the data to a common format in a YAML config file. However this seems overly complicated and hard to generalize; chose to implement a subclass of DatasetDownloader for each dataset we want to use instead.
    
    * Fixed potential bug in case LOCALAPPDATA environment variable is not defined in a Windows environment.
    
    * Removed some dead imports.
    
    * Added `image_helpers.py` module for miscellaneous custom functions regarding images (e.g. an 'file_is_image' check).
    
    * Added dataset loading for EPLV dataset. (loading of labels is not completely functional and there are no tests written yet...).

--- tmap_defectdetector/datasets.py
+++ tmap_defectdetector/datasets.py
@@ -1,16 +1,26 @@
 from __future__ import annotations
 
+import csv
+import re
 from abc import ABC, abstractmethod
 import inspect
+from pathlib import Path
 from typing import TypeAlias, Iterable
 
+import pandas as pd
 from PIL.Image import Image
 
+from tmap_defectdetector.logger import log
+
 ImageCollection: TypeAlias = list[Image] | tuple[Image, ...]
-LabelCollection: TypeAlias = list[int | float | str] | tuple[int | float | str]
+LabelCollection: TypeAlias = list[pd.DataFrame] | tuple[pd.DataFrame, ...]
 Translation: TypeAlias = tuple[float, float] | tuple[int, int]
 
+
 class AbstractDataSet(ABC):
+
+    label_data: list[pd.DataFrame]
+
     @abstractmethod
     def amplify_data(self):
         """
@@ -22,38 +32,44 @@ class AbstractDataSet(ABC):
         """
         pass
 
+    @abstractmethod
+    def add_label_data(self, label_path_or_data: Path | pd.DataFrame):
+        ...
 
-class DataSet(ABC):
     def amplify_data(self, *args, **kwargs):
         ...
 
 
-class ImageDataSet(DataSet):
+class ImageDataSet(AbstractDataSet):
 
     _VALID_MIRROR_AXES: tuple[int, ...] = (0, 45, 90, 135, 180, 225, 270, 360)
-    DEFAULT_TRANSLATIONS: Iterable[Translation] = ((.5, .5), (.5, -.5), (-.5, -.5), (-.5, .5))
+    DEFAULT_TRANSLATIONS: Iterable[Translation] = (
+        (0.5, 0.5),
+        (0.5, -0.5),
+        (-0.5, -0.5),
+        (-0.5, 0.5),
+    )
     DEFAULT_ROTATIONS: Iterable[int] = (90, 180, 270)
     DEFAULT_MIRROR_AXES: Iterable[float | int] = (0, 45, 90, 135)
 
-    def __init__(self,
-                 data: ImageCollection = (),
-                 labels: LabelCollection = ()):
+    def __init__(self, data: ImageCollection = tuple(), label_data: list[pd.DataFrame] = tuple()):
         self.data: list[Image] = list(data)
-        self.labels = labels
+        self.label_data = list(label_data)
 
-    def amplify_data(self,
-                     translations: Iterable[float | int] = DEFAULT_TRANSLATIONS,
-                     rotations: Iterable[float | int] = DEFAULT_ROTATIONS,
-                     mirror_axes: Iterable[float | int] = DEFAULT_MIRROR_AXES,
-                     *args, **kwargs
-                     ):
+    def amplify_data(
+        self,
+        translations: Iterable[float | int] = DEFAULT_TRANSLATIONS,
+        rotations: Iterable[float | int] = DEFAULT_ROTATIONS,
+        mirror_axes: Iterable[float | int] = DEFAULT_MIRROR_AXES,
+        *args,
+        **kwargs,
+    ):
         for translation in translations:
             self.data += self.generate_translation(translation[0], translation[1])
         for rotation in rotations:
             self.data += self.generate_rotation(rotation)
         for cur_axis in mirror_axes:
             self.data += self.generate_mirrors(axis=cur_axis)
-
 
     def generate_mirrors(self, axis: float | int) -> list[Image]:
         """
@@ -92,6 +108,28 @@ class ImageDataSet(DataSet):
             f"{type(self).__name__}.{inspect.currentframe().f_code.co_name}' implementation is not finished yet."
         )
 
+    def add_label_data(self, label_path_or_data: Path | pd.DataFrame):
+        match label_path_or_data:
+            case Path():
+                if label_path_or_data.suffix.lower() in (accepted_extensions := (".csv", ".tsv")):
+
+                    clean_whitespace(label_path_or_data)
+
+                    data_lines = label_path_or_data.read_text().split(" ")
+                    dialect = csv.Sniffer().sniff(data_lines[0])
+                    self.label_data.append(pd.read_csv(label_path_or_data, sep=dialect.delimiter))
+                else:
+                    raise NotImplementedError(
+                        f"Label data handling for types other than {', '.join(accepted_extensions)} not (yet) implemented."
+                    )
+            case pd.DataFrame():
+                self.label_data.append(label_path_or_data)
+            case _:
+                raise TypeError(
+                    f"Expected type {pd.DataFrame.__name__} or {Path.__name__}, "
+                    f"got {type(label_path_or_data)}."
+                )
+
     def __len__(self) -> int:
         return len(self.data)
 
@@ -102,16 +140,52 @@ class ImageDataSet(DataSet):
                 if all(isinstance(item, Image) for item in other):
                     self.data += list(other)
                 else:
-                    raise TypeError(f"All items in data to be added must be images ({Image.__qualname__} objects),"
-                                    " which seems not to be the case.")
+                    raise TypeError(
+                        f"All items in data to be added must be images ({Image.__qualname__} objects),"
+                        " which seems not to be the case."
+                    )
             case ImageDataSet():
                 self.data += other.data
             case Image():
                 self.data.append(other)
             case _:
-                raise TypeError(f"Can only add together (lists of) images or other {type(self).__name__!r} "
-                                f"datasets to this {type(self).__name__} dataset.")
+                raise TypeError(
+                    f"Can only add together (lists of) images or other {type(self).__name__!r} "
+                    f"datasets to this {type(self).__name__} dataset."
+                )
         return self
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}(data={self.data.__repr__()})"
+
+
+def clean_whitespace(
+    filepath: Path,
+    whitespace_search_regex: re.Pattern = re.compile(r"\s+"),
+    replacement: str = " ",
+) -> None:
+    """
+    Removes inconsistent whitespaces in CSV files (e.g. single space for 1st column, multiple for next like in the ELPV dataset)
+    and replaces them with singular ones.
+
+    :param filepath: Path to clean whitespaces for.
+    :param whitespace_search_regex: regular expression used to match whitespace (as re.Pattern object).
+    :param replacement: string to replace whitespaces with.
+    """
+
+    newlines = []
+    for sep in ("\n", "\r\n", "\r"):
+        if len(filepath.read_text(encoding="utf-8").split("\r\n")) == 1:
+            log.debug(
+                "Huh!?!!?!?! labels.csv from ELPV dataset has no newlines but they are in my text editor? Weird..."
+            )
+
+    # with open(filepath, "r") as textfile:
+    #    for line in textfile.readlines():
+    #        newlines.append(whitespace_search_regex.sub(replacement, line).strip())
+    # newlines = [x for y in [line.split(" ") for line in newlines] for x in y]
+    # with open(Path(filepath.parent, filepath.stem + "_clean" + filepath.suffix), "w") as newfile:
+
+    #    newfile.writelines(newlines)
+    # print(f"Temporary breakpoint in {__name__}")
+    # raise NotImplementedError("Somehow the newline characters aren't recognized in the ELPV dataset...")
