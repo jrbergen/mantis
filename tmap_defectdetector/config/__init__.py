042a1098af2d75fb82ec66a20e3201c9568f0fda
Ashwin Padmashali <ash.padmashali@gmail.com>
2022-04-19 15:43:46 +0200

    removed import tensorflow from __init__.py

--- tmap_defectdetector/config/__init__.py
+++ tmap_defectdetector/config/__init__.py
@@ -1,7 +1,2 @@
 from __future__ import annotations
-<<<<<<< HEAD:src/tmap-defectdetector/main.py
 
-import tensorflow as tf
-
-=======
->>>>>>> 8f428de13b19ac864ddfb6d1beb289467397df15:tmap_defectdetector/config/__init__.py
