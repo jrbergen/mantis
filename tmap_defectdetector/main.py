eaf3deafd125a27813d2d9a0166cd409fe492a08
jrbergen <joost@bergensoftware.nl>
2022-04-20 21:38:01 +0200

    Changed `main.py` to make use of simplifications.

--- tmap_defectdetector/main.py
+++ tmap_defectdetector/main.py
@@ -1,8 +1,16 @@
+"""The main file containing the program's entrypoint."""
 from __future__ import annotations
 
+from pathlib import Path
+
+from tqdm import tqdm
 
 from tmap_defectdetector.compatibility_checks import version_check
-from tmap_defectdetector.dataset_loading import DatasetDownloaderELPV
+from tmap_defectdetector.config.paths import DIR_DATASETS
+from tmap_defectdetector.dataset_downloaders import DatasetDownloaderELPV
+
+from tmap_defectdetector.datasets import ImageDataSetELPV
+from tmap_defectdetector.image_helpers import file_is_image
 from tmap_defectdetector.logger import log
 
 
@@ -13,17 +21,32 @@ def cli():
 def main():
     version_check()
 
-    # Just an example for now demonstrating dataset downloading, this will not be in main eventually
-    dataset_downloader = DatasetDownloaderELPV()
-    dataset_downloader.download()
-    dataset = dataset_downloader.load()
+    # Download the ELPV dataset from its git repository
+    DatasetDownloaderELPV().download(Path(DIR_DATASETS, "dataset-elpv"))
+    # The dataset is downloaded to %LOCALAPPDATA%/.tmapdd/datasets/dataset-elpv/ (on Windows)
 
-    print(
-        f"vv Example of loaded labels: vv",
-        *[dframe.info() for dframe in dataset.label_data],
-        f"^^ Example of loaded label dataframe(s) ^^",
-        sep="\n",
-    )
+    # We first find the image paths and label file Path(s) before we construct the ImageDataSet.
+    # This should probably be made part of a method in the DatasetDownloader which gives the label/data paths.
+    label_paths = [Path(DIR_DATASETS, "dataset-elpv", "labels.csv")]
+
+    image_dirs = [Path(DIR_DATASETS, p) for p in [Path("dataset-elpv", "images")]]
+
+    log.info("Loading images...")
+    data_paths = []
+    # Currently this crudely finds all image files in any of the (sub)directories in the image_dirs variable.
+    for imgdir in image_dirs:
+        files = tuple(imgdir.rglob("*.*"))
+        for file in tqdm(files, desc="Finding images...", unit="images", total=len(files)):
+            if file.is_file() and file_is_image(file):
+                data_paths.append(file)
+
+    # Now we know where the label path(s) and the data (image file) paths are, so we can construct the ImageDataSet.
+    dataset = ImageDataSetELPV.from_paths(data_paths=data_paths, label_paths=label_paths)
+
+    # Here comes the preprocessing step (we could e.g. make a ImageDataSetPreProcessor class/function or perhaps
+    # put preprocessing methods in the ImageDataSet class itself later.
+    dataset.amplify_data()
+
     log.info("All done!")
 
 
