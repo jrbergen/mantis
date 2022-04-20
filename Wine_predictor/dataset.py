d76def7e25f4cf297b183644e9807979d62e3115
Ashwin Padmashali <ash.padmashali@gmail.com>
2022-04-20 15:41:12 +0200

    wine predictor OOP

---
+++ Wine_predictor/dataset.py
@@ -0,0 +1,31 @@
+# Handles Wine data
+
+import pandas as pd
+from pathlib import Path
+
+class WineData:
+    def __init__(self):
+        self.path = Path(__file__).parent.parent / "data/winequality-red.csv"
+        self.df                 = None
+
+        # input factors
+        self.fixed_acidity      = None
+        self.volatile_acidity   = None
+        self.citric_acid        = None
+        self.residual_sugar     = None
+        self.chlorides          = None
+        self.free_SO2           = None
+        self.total_SO2          = None
+        self.density            = None
+        self.sulphates          = None
+        self.alcohol            = None
+
+        # output factor
+        self.quality            = None
+
+    def ImportData(self):
+        # Function to import csv file
+        # and run initial file changes to create good data
+        self.df = pd.read_csv(self.path)
+        #print("read")
+        self.df = self.df.rename(columns={'free sulfur dioxide': 'free_SO2'})
---
+++ Wine_predictor/main.py
@@ -0,0 +1,41 @@
+# import standard libraries
+
+from __future__ import annotations
+
+# pandas import already in dataset
+import matplotlib.pyplot as plt
+import seaborn as sn
+import numpy as np
+
+# sklearn for working with models and regression
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import metrics
+from sklearn.metrics import classification_report, confusion_matrix
+
+#import custom data types and libraries
+import dataset
+
+def AssignXY(wdata):
+    # Function to allocate for X and Y values
+    # for linear regression
+    X = wdata.pH
+    Y = wdata.quality
+    print (X)
+    sn.scatterplot(X)     # Visualize Data
+
+def main():
+    print("running")
+    winedata = dataset.WineData()       # Create object for operating on data
+    #print(winedata.path)
+    
+    winedata.ImportData()               # Imported Data from csv format 
+    #print(winedata.df)                 # winedata.df stores all data
+
+    AssignXY(winedata.df)
+
+if __name__ == "__main__":
+    print ("Running")
+    main()
+    #run something
