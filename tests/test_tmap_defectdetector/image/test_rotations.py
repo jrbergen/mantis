Corrected erroneously named tests for image rotation.

tests/test_tmap_defectdetector/image/test_rotations.py
@@ -20,12 +20,12 @@ IMG33_ROT_90 = np.array([[3, 6, 9], [2, 5, 8], [1, 4, 7]])
 IMG33_ROT_180 = np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
 IMG33_ROT_270 = np.array([[7, 4, 1], [8, 5, 2], [9, 6, 3]])
 
-NON_SQUARE_1 = np.array([1, 2])
-NON_SQUARE_2 = np.array([[1, 2, 3], [4, 5, 6]])
-NON_SQUARE_3 = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
-NON_SQUARE_4 = np.array([[1, 2], [3, 4], [5, 6]])
-NON_SQUARE_5 = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
-NON_SQUARES: tuple[ndarray, ...] = (NON_SQUARE_1, NON_SQUARE_2, NON_SQUARE_3, NON_SQUARE_4, NON_SQUARE_5)
+NON_SQUARE_0 = np.array([1, 2])
+NON_SQUARE_1 = np.array([[1, 2, 3], [4, 5, 6]])
+NON_SQUARE_2 = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+NON_SQUARE_3 = np.array([[1, 2], [3, 4], [5, 6]])
+NON_SQUARE_4 = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+NON_SQUARES: tuple[ndarray, ...] = (NON_SQUARE_0, NON_SQUARE_1, NON_SQUARE_2, NON_SQUARE_3, NON_SQUARE_4)
 
 
 class RotateTestParams(NamedTuple):
@@ -47,7 +47,7 @@ class TestRotateImg:
         ),
         ids=[f"{ndeg} degree rotation {dim}x{dim}" for ndeg, dim in itertools.product((90, 180, 270), (2, 3))],
     )
-    def test_rotate_square_deg(self, img, degrees, expected):
+    def test_rotate_img_deg(self, img, degrees, expected):
         np.testing.assert_equal(rotate_img(img, degrees), expected)
 
     @pytest.mark.parametrize(
@@ -60,7 +60,7 @@ class TestRotateImg:
         ids=[f"Single 1x1 pixel image; {ndeg} degree rotation 1x1" for ndeg in (90, 180, 270)],
     )
     @pytest.mark.filterwarnings("ignore")
-    def test_rotate_square_single_value_img(self, img, degrees, expected):
+    def test_rotate_img_single_value(self, img, degrees, expected):
         np.testing.assert_equal(rotate_img(img, degrees), expected)
 
     @pytest.mark.parametrize(
@@ -73,15 +73,14 @@ class TestRotateImg:
         ids=[f"Empty 0x0 image; {ndeg} degree rotation" for ndeg in (90, 180, 270)],
     )
     @pytest.mark.filterwarnings("ignore")
-    def test_rotate_square_empty_img(self, img, degrees, expected):
-        with pytest.warns(UserWarning, match=r".*empty image.*"):
-            np.testing.assert_equal(rotate_img(img, degrees), expected)
+    def test_rotate_img_empty(self, img, degrees, expected):
+        np.testing.assert_equal(rotate_img(img, degrees), expected)
 
-    def test_rotate_square_single_value_img_warning(self):
+    def test_rotate_img_single_value_warning(self):
         with pytest.warns(UserWarning, match=r".*singular image.*"):
             rotate_img(IMG11, 90)
 
-    def test_rotate_square_empty_img_warning(self):
+    def test_rotate_img_empty_warning(self):
         with pytest.warns(UserWarning, match=r".*empty image.*"):
             rotate_img(IMG00, 90)
 
@@ -100,6 +99,6 @@ class TestRotateImgSquare(TestRotateImg):
             for curi, (ndeg, arr) in enumerate(itertools.product((0, 90), NON_SQUARES))
         ],
     )
-    def test_rotate_square_nonsquare_mat(self, img, degrees, expected):
+    def test_rotate_img_square_nonsquare_error(self, img, degrees, expected):
         with pytest.raises(ValueError):
             rotate_img_square(img, degrees)