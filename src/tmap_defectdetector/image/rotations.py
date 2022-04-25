4e8598b604b20bb67e2bc6e90b6adc4605f072da
jrbergen <joost@bergensoftware.nl>
2022-04-25 21:35:32 +0200

    Added function `rotate_img`. Renamed function `rotate_square` to `rotate_img_square`.

--- src/tmap_defectdetector/image/rotations.py
+++ src/tmap_defectdetector/image/rotations.py
@@ -1,16 +1,29 @@
 """Functions for rotating images."""
 import inspect
+import warnings
 
 import numpy as np
 from numpy import ndarray
 
 
-def rotate_square(img: ndarray, degrees: int) -> ndarray:
+def rotate_img(img: ndarray, degrees: int) -> ndarray:
     """
     Rotates image (as numpy array) with multiples of 90 degrees / .5pi rad.
 
     :raises ValueError: if degrees is not a multiple of 90 or 0.
+    :raises ValueError: if image is not square.
     """
+
+    if img.shape == (1,):
+        warnings.warn(
+            "Tried to rotate singular image (image with 1 pixel). Returned image unaltered.", UserWarning
+        )
+        return img
+
+    if img.size == 0:
+        warnings.warn("Tried to rotate empty image. Returned image unaltered.", UserWarning)
+        return img
+
     degrees %= 360
     match degrees:
         case 0:
@@ -26,3 +39,23 @@ def rotate_square(img: ndarray, degrees: int) -> ndarray:
                 f"Function {inspect.currentframe().f_code.co_name} only "
                 f"allows rotation in multiples of 90 degrees (.5pi rad)."
             )
+
+
+def rotate_img_square(img: ndarray, degrees: int) -> ndarray:
+    """
+    Rotates image (as numpy array) with multiples of 90 degrees / .5pi rad.
+
+    :raises ValueError: if degrees is not a multiple of 90 or 0.
+    :raises ValueError: if image is not square.
+    """
+
+    if img.size > 0 and (len(img.shape) <= 1 or img.shape[0] != img.shape[1]):
+        raise ValueError(f"Expected square image/matrix; got shape {img.shape!r}.")
+
+    return rotate_img(img=img, degrees=degrees)
+
+
+if __name__ == "__main__":
+    a = rotate_img_square(np.array([1, 2]), 90)
+
+    print(f"Temporary breakpoint in {__name__}")
